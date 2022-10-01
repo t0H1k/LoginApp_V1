3b2657074a35ac6f6e2b9f1f9991a9713fa6bf85
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-10-01 23:44:31 +0300

    change background color to gradient color

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -17,6 +17,7 @@ final class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         emojiLabel.text = "👻"
+        setGradientBackground()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -27,5 +28,17 @@ final class WelcomeViewController: UIViewController {
     @IBAction func logOutButtonTapped() {
         dismiss(animated: true)
         
+    }
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 012/255, green: 149/255, blue: 0/255, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255/255, green: 94/255, blue: 58/255, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
     }
 }
